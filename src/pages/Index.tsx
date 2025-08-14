2d6bf026ac0d753bd1b87514bdc10da6742d47fb
nidhi-subrah <nidhisubrahmanya06@gmail.com>
2025-08-14 16:36:58 -0400

    Added About Me Section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 
 import Hero from "@/components/Hero";
 import Summary from "@/components/Summary";
+import AboutMe from "@/components/AboutMe";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
@@ -11,6 +12,7 @@ const Index = () => {
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-purple-900">
       <Navigation />
       <Hero />
+      <AboutMe />
       <Summary />
       <Experience />
       <Projects />
